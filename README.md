13f75e75719fa03a5b9a54a1aeea332ad403055e
a-bakos <bakosattila@fastem.com>
2023-10-06 17:41:11 +0100

    Add plugin frame

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# wdi
+# WannaKnow?
---
+++ core/index.php/WK.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: WannaKnow?
+ * Description: Investigate admin-side events
+ * Version: 0.1.0
+ * Author: Attila Bakos
+ */
+
+ // If this file is called directly, abort
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
