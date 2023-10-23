f25f6ba61bd2d790434565c4de22278879df3f56
a-bakos <bakosattila@fastem.com>
2023-10-23 22:12:19 +0100

    Add placeholder log insertion to Media Uploaded event

--- core/WK_Event_Listener_Media.php
+++ core/WK_Event_Listener_Media.php
@@ -17,6 +17,23 @@ readonly final class WK_Event_Listener_Media implements WK_Consts {
 		// Filter $_POST array for security
 		$post_array = filter_input_array( INPUT_POST );
 
+		$user_data = []; // todo placeholder
+
+		$media_id = 0;
+
+		return ( new WK_DB() )?->insert_log_item( WK_DB::prepare_log_item(
+			user_id:           $user_data->data->ID ?? self::UNKNOWN_ID,
+			event_id:          WK_Event::FILE_UPLOADED->value,
+			subject_id:        $media_id,
+			subject_type:      WK_Subject_Type::File->value,
+			subject_title:     '',
+			subject_url:       '',
+			subject_old_value: '',
+			subject_new_value: '',
+			description:       '',
+			user_email:        $user_data->data->user_email,
+		) );
+
 		return false;
 	}
 
