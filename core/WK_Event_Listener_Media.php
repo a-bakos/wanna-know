cf64f05959645eb15eea41cdb8b3ca7bf00ed16e
a-bakos <bakosattila@fastem.com>
2023-10-25 20:54:15 +0100

    Add comments

--- core/WK_Event_Listener_Media.php
+++ core/WK_Event_Listener_Media.php
@@ -17,12 +17,50 @@ readonly final class WK_Event_Listener_Media extends WK_Current_User implements 
 		// Filter $_POST array for security
 		$post_array = filter_input_array( INPUT_POST );
 
-		if ( isset( $post_array['action'] ) && $post_array['action'] === \WK_Action_Type::UPLOAD_ATTACHMENT ) {
+		$key_action = 'action';
+		if ( isset( $post_array[ $key_action ] ) && $post_array[ $key_action ] === \WK_Action_Type::UPLOAD_ATTACHMENT ) {
 			$user_data = self::get_userdata();
 
-			//$file = get_attached_file( $attachment_id );
-			//wk_p( $file );
-			//die;
+//			$file = get_attached_file( $attachment_id );
+//			var_dump( $file );
+			/*
+						{"success":true,
+							"data":{
+							"id":23,
+							"title":"profitient-training-logo",
+							"filename":"profitient-training-logo.png",
+							"url":"http:\/\/localhost\/wp\/wp-content\/uploads\/2023\/10\/profitient-training-logo.png",
+							"link":"http:\/\/localhost\/wp\/profitient-training-logo\/",
+							"alt":"",
+							"author":"1",
+							"description":"",
+							"caption":"",
+							"name":"profitient-training-logo",
+							"status":"inherit",
+							"uploadedTo":0,
+							"date":1698263274000,
+							"modified":1698263274000,
+							"menuOrder":0,
+							"mime":"image\/png",
+							"type":"image",
+							"subtype":"png",
+							"dateFormatted":"October 25, 2023",
+							"editLink":"http:\/\/localhost\/wp\/wp-admin\/post.php?post=23&action=edit",
+
+							"authorName":"abakos",
+							"authorLink":"http:\/\/localhost\/wp\/wp-admin\/profile.php",
+							"filesizeInBytes":188901,
+							"filesizeHumanReadable":"184 KB",
+							"context":"",
+							"height":1348,
+							"width":3988,
+							"orientation":"landscape",
+							"sizes":{
+								"full":{
+									"url":"http:\/\/localhost\/wp\/wp-content\/uploads\/2023\/10\/profitient-training-logo.png",
+									"height":1348,
+									"width":3988,
+									*/
 
 			return ( new WK_DB() )?->insert_log_item( WK_DB::prepare_log_item(
 				user_id:           $user_data['ID'] ?? self::UNKNOWN_ID,
@@ -42,6 +80,12 @@ readonly final class WK_Event_Listener_Media extends WK_Current_User implements 
 	}
 
 	public function media_deleted(): bool {
+
+		// todo
+		// on delete request, grab ID, filename
+		// after delete, go and try to find the ID on a media upload event
+		// and connect the deleted log item to that event
+
 		return false;
 	}
 }
