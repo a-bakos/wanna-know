1d959d48a4d78cb206c5ecfd1b0e340f9edaad8e
a-bakos <bakosattila@fastem.com>
2023-11-12 17:28:16 +0000

    Category event listener placeholder methods

--- core/WK_Event_Listener_Category.php
+++ core/WK_Event_Listener_Category.php
@@ -9,6 +9,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 readonly final class WK_Event_Listener_Category {
 	public function __construct() {
-		//
+		add_action( 'create_category', [ $this, 'category_created' ], 10, 1 );
+		add_action( 'edit_category', [ $this, 'category_edited' ], 10, 1 );
+		add_action( 'delete_term_taxonomy', [ $this, 'term_deleted' ], 10, 1 );
 	}
+
+	public function category_created() {}
+
+	public function category_edited() {}
+
+	public function term_deleted() {}
+
 }
